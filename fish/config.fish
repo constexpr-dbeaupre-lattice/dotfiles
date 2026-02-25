b0a4eeb3feee62939773bfe5df5ae1f4c91e091b
David-Alexandre Beaupre <david-alexandre.beaupre@latticesemi.com>
2026-02-25 08:29:19 -0500

    Setup keychain for easier SSH keys management.

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,7 @@ if status is-interactive
         tmux attach-session -t default; or tmux new-session -s default
     end
 	source "$HOME/.cargo/env.fish"
+    keychain -q --eval ~/.ssh/id_ed25519 | source
 end
+
+source /home/beaupreda/miniconda3/etc/fish/conf.d/conda.fish
